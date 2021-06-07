a96816d04890d7d4ef6ce21c18766ab99092e1aa
abraham.lloyd <abraham.lloyd@salesforce.com>
2021-06-07 13:57:29 -0400

    #117 ... updated the target build number.

--- config/default.js
+++ config/default.js
@@ -1,7 +1,7 @@
 module.exports = {
 
     // Describes the current versionNo of the service-cloud-connector
-    "versionNo": "0.7.9",
+    "versionNo": "0.8.0",
 
     // Manages debug mode for the CLI application
     "debugMode": true,
