8beebe64008ad7556887c3332a025cac142d2a4e
abraham.lloyd <abraham.lloyd@salesforce.com>
2021-07-01 21:55:10 -0400

    #137 ... second-pass at esLint rules for b2c-crm-sync cartridges (targeting plugins and seeing the impact via static analysis).

--- src/sfcc/cartridges/int_b2ccrmsync/cartridge/scripts/models/authToken.js
+++ src/sfcc/cartridges/int_b2ccrmsync/cartridge/scripts/models/authToken.js
@@ -1,7 +1,14 @@
+/* global request: false empty: false */
 'use strict';
 
 /**
  * @module models/authToken
+ */
+
+/**
+ * @typedef  {Object} tokenObj Represents the tokenObject provided by the Salesforce Platform
+ * @property {String} access_token Describes if the current customer represents a newCustomer
+ * @property {String} issued_at Describes if the current customer represents a newCustomer
  */
 
 /**
@@ -15,7 +22,6 @@ var EXPIRE_LIMIT = 10 * 60 * 1000;
  * Returns whether the stored token is valid (exist and not expired)
  *
  * @param {Object} tokenObj The token object retrieved from the auth API response
- *
  * @returns {Boolean} Whether the token is valid and not expired
  */
 function isValidAuth(tokenObj) {
@@ -31,21 +37,29 @@ function isValidAuth(tokenObj) {
  * Gets a valid token from storage or from a new auth request
  *
  * @param {Boolean} bypassCache If true, then the authentication token will be retrieved from the Salesforce Core Platform, not from the cache
- *
  * @returns {Object} Plain JS object containing the token response. If the token is not valid or not found, throw an error
  */
 function getValidToken(bypassCache) {
-    var cache = require('dw/system/CacheMgr').getCache(require('../b2ccrmsync.config').customCacheID);
+
+    // Initialize local variables
+    var cache,
+        cacheKey,
+        tokenObj;
+
+    // Pull the cache using the customCacheKey
+    cache = require('dw/system/CacheMgr').getCache(require('../b2ccrmsync.config').customCacheID);
+
     // Cache the token per site, per locale
     // This is useful in case the integration is per country
-    var cacheKey = [require('dw/system/Site').getCurrent().getID(), request.locale, 'auth'].join('_');
+    cacheKey = [require('dw/system/Site').getCurrent().getID(), request.locale, 'auth'].join('_');
 
     // In case we need to bypass the cache, let's remove the value from the cache, so that the cache loader function will be executed
     if (bypassCache) {
         cache.invalidate(cacheKey);
     }
 
-    var tokenObj = cache.get(cacheKey, function requestAuthToken() {
+    // Retrieve the token from the in-memory cache
+    tokenObj = cache.get(cacheKey, function requestAuthToken() {
         // In case the token does not exist in the cache, then retrieve it from the API, and store it
         var svc = require('../services/ServiceMgr').getAuthService();
         var result = svc.call();
@@ -54,14 +68,19 @@ function getValidToken(bypassCache) {
             return result.object;
         }
 
+        // Exit early
         return undefined;
+
     });
 
+    // Was an authToken found?  If not, throw an error
     if (!isValidAuth(tokenObj)) {
         throw new Error('No auth token available, please verify your configuration!');
     }
 
+    // Return the tokenObject
     return tokenObj;
+
 }
 
 module.exports.getValidToken = getValidToken;
--- src/sfcc/cartridges/int_b2ccrmsync/cartridge/scripts/services/ServiceMgr.js
+++ src/sfcc/cartridges/int_b2ccrmsync/cartridge/scripts/services/ServiceMgr.js
@@ -1,3 +1,4 @@
+/* global request:false */
 'use strict';
 
 /**
